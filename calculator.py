20f731754337f678a07036ba423e943ebacc3e63
Ian Chen <ianchen@IandeMacBook-Air.local>
2022-07-02 21:49:19 +0800

    fix function name typo

--- calculator.py
+++ calculator.py
@@ -7,8 +7,7 @@ def add(first_item, second_item):
 	return first_item + second_item 
 
 
-def substract(first_item, second_item): 
+def subtract(first_item, second_item): 
 
 	return first_item - second_item 
 
-	
